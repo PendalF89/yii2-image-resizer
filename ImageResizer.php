833accf5426b88b8c731f89555f8bcd0d2c2961b
PendalF <zabolotskich@bk.ru>
2016-04-18 00:08:28 +0300

    Create ImageResizer class and update readme.

--- ImageResizer.php
+++ ImageResizer.php
@@ -2,19 +2,30 @@
 
 namespace pendalf89\imageresizer;
 
+use Imagine\Image\ManipulatorInterface;
 use Yii;
 use yii\base\Component;
 use RecursiveIteratorIterator;
 use RecursiveDirectoryIterator;
+use yii\imagine\Image;
 
+/**
+ * Class ImageResizer
+ * Class for creation thumbs from original image.
+ *
+ * @package pendalf89\imageresizer
+ */
 class ImageResizer extends Component
 {
 	/**
 	 * @var array image sizes
+	 * For example:
+	 *  [
+	 *		['width' => 300, 'height' => 200],
+	 *		['width' => 300, 'height' => 100],
+	 *	]
 	 */
-	public $sizes = [
-		['width' => 300, 'height' => 200],
-	];
+	public $sizes;
 
 	/**
 	 * @var string directory with images
@@ -26,26 +37,135 @@ class ImageResizer extends Component
 	 */
 	public $recursively = true;
 
-	public function runResize()
-	{
+	/**
+	 * @var bool enable rewrite thumbs if its already exists
+	 */
+	public $enableRewrite = false;
 
+	/**
+	 * @var array|string the driver to use. This can be either a single driver name or an array of driver names.
+	 * If the latter, the first available driver will be used.
+	 *
+	 * For more information see yii\imagine\BaseImage
+	 */
+	public $driver = [Image::DRIVER_GMAGICK, Image::DRIVER_IMAGICK, Image::DRIVER_GD2];
+
+	/**
+	 * @var string image creation mode.
+	 * For more information see Imagine\Image\ManipulatorInterface
+	 */
+	public $mode = ManipulatorInterface::THUMBNAIL_OUTBOUND;
+
+	/**
+	 * @var bool enable to delete all images, which sizes not in $this->sizes array
+	 */
+	public $deleteNonActualSizes = true;
+
+	/**
+	 * Run image resizing.
+	 */
+	public function run()
+	{
+		$this->setSizesPostfixes();
+		$filenames = $this->collectFilenames();
+
+		if ($this->deleteNonActualSizes) {
+			$this->cleanDirectory($filenames);
+		}
+
+		foreach ($filenames as $filename) {
+			if ($this->isOriginal($filename)) {
+				$this->resize($filename);
+			}
+		}
 	}
 
-	protected function collectFilenamesForResize()
+	/**
+	 * Resize and save new images.
+	 * If rewrite disabled and filename already exists, than skip.
+	 *
+	 * @param string $filename image filename
+	 */
+	protected function resize($filename)
+	{
+		Image::$driver = $this->driver;
+
+		foreach ($this->sizes as $size) {
+			$newFilename = $this->addPostfix($filename, $size['postfix']);
+
+			if (!$this->enableRewrite && file_exists($newFilename)) {
+				continue;
+			}
+
+			Image::thumbnail($filename, $size['width'], $size['height'], $this->mode)->save($newFilename);
+		}
+	}
+
+	/**
+	 * Add postfix to filename
+	 *
+	 * @param string $filename
+	 * @param string $postfix
+	 *
+	 * @return string
+	 */
+	protected function addPostfix($filename, $postfix)
+	{
+		$pathinfo = pathinfo($filename);
+		return "$pathinfo[dirname]/$pathinfo[filename]$postfix.$pathinfo[extension]";
+	}
+
+	/**
+	 * Collect images filenames from dir
+	 *
+	 * @return array
+	 */
+	protected function collectFilenames()
 	{
 		$dir = $this->getDirectory();
 		$filenames = [];
 
 		if ($this->recursively) {
-			foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $filename) {
-				if (!$filename->isDir()) {
-					$filenames[] = $filename;
+			foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $file) {
+				if (!$file->isDir()) {
+					$filenames[] = $file->getPathName();
 				}
 			}
 		} else {
-
+			$filenames = scandir($dir);
+			foreach ($filenames as $key => $filename) {
+				$filename = "$dir/$filename";
+				if (is_dir($filename)) {
+					unset($filenames[$key]);
+				} else {
+					$filenames[$key] = $filename;
+				}
+			}
 		}
 
+		return $filenames;
+	}
+
+	/**
+	 * If filename doesn`t match mask "-123x123.", than this original filename.
+	 *
+	 * @param string $filename
+	 *
+	 * @return bool
+	 */
+	protected function isOriginal($filename)
+	{
+		return !(boolean) preg_match('/-\d+x\d+\./u', $filename);
+	}
+
+	/**
+	 * Set postfixes to sizes array
+	 */
+	protected function setSizesPostfixes()
+	{
+		foreach ($this->sizes as $key => $size) {
+			$this->sizes[$key]['postfix'] = "-$size[width]x$size[height]";
+		}
 	}
 
 	/**
@@ -56,5 +176,19 @@ class ImageResizer extends Component
 	protected function getDirectory()
 	{
 		return Yii::getAlias($this->dir);
+	}
+
+	/**
+	 * Delete all non-original images.
+	 *
+	 * @param array $filenames
+	 */
+	protected function cleanDirectory($filenames)
+	{
+		foreach ($filenames as $filename) {
+			if (!$this->isOriginal($filename)) {
+				unlink($filename);
+			}
+		}
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# yii2-image-resizer
+Yii2 image resizer
+================
+This Yii2 component provide creation thumbnails from original image.
+Just add array with sizes and have fun!
+
+Installation
+------------
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist pendalf89/yii2-image-resizer "*"
+```
+
+or add
+
+```
+"pendalf89/yii2-image-resizer": "*"
+```
+
+to the require section of your `composer.json` file.
+
+Configuration:
+
+```php
+'components' => [
+    'imageResizer' => [
+        'class'                => 'pendalf89\imageresizer\ImageResizer',
+        // directory with images
+        'dir'                  => '@runtime/images',
+        // image sizes
+        'sizes'                => [
+            ['width' => 300, 'height' => 200],
+            ['width' => 150, 'height' => 150],
+        ],
+        // handle directory recursively
+        'recursively'          => true,
+        // enable rewrite thumbs if its already exists
+        'enableRewrite'        => false,
+        // array|string the driver to use. This can be either a single driver name or an array of driver names.
+        // If the latter, the first available driver will be used.
+        'driver'               => ['gd2', 'imagick', 'gmagick'],
+        // image creation mode.
+        'mode'                 => 'outbound',
+        // enable to delete all images, which sizes not in $this->sizes array
+        'deleteNonActualSizes' => false,
+    ],
+],
+```
+
+Usage
+------------
+
+```php
+Yii::$app->imageResizer->run();
+```
